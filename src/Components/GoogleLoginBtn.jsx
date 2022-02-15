90338d9f2337448ec452f258d30e253aed621646
Taylor <trm313@gmail.com>
2022-02-15 13:38:22 -0500

    Pared down Google scope, added an error state for login

--- src/Components/GoogleLoginBtn.jsx
+++ src/Components/GoogleLoginBtn.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { GoogleLogin } from "react-google-login";
 import { Flex, Heading, Text, Icon } from "@chakra-ui/react";
@@ -10,39 +10,49 @@ https://github.com/anthonyjgrove/react-google-login
 https://dev.to/sivaneshs/add-google-login-to-your-react-apps-in-10-mins-4del
 */
 
-const responseGoogle = (response) => {
-  console.log(response);
-};
-
 const GoogleLoginBtn = ({ onLogin }) => {
+  const [error, setError] = useState(null);
+
+  const onSuccess = (response) => {
+    setError(null);
+    onLogin(response);
+  };
+  const onFailure = (response) => {
+    console.log(response);
+    setError("There was an issue authenticating to Google, please try again");
+  };
+
   return (
-    <GoogleLogin
-      clientId='12032474523-j8qabilh90gi615h4luvhc608aq4ar8n.apps.googleusercontent.com'
-      buttonText='Connect to Google'
-      onSuccess={onLogin}
-      onFailure={responseGoogle}
-      cookiePolicy={"single_host_origin"}
-      isSignedIn={true}
-      scope='profile email https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/forms.responses.readonly'
-      render={(renderProps) => (
-        // <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
-        <Flex
-          onClick={renderProps.onClick}
-          disabled={renderProps.disabled}
-          cursor='pointer'
-          alignItems='center'
-          justifyContent='center'
-          maxW='sm'
-          p='3'
-          borderWidth='2px'
-          rounded='md'
-          mt='4'
-        >
-          <Icon as={FcGoogle} mr={4} w={10} h={10} />
-          <Heading size='md'>Connect to Google</Heading>
-        </Flex>
-      )}
-    />
+    <>
+      <GoogleLogin
+        clientId='12032474523-j8qabilh90gi615h4luvhc608aq4ar8n.apps.googleusercontent.com'
+        buttonText='Connect to Google'
+        onSuccess={onLogin}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+        scope='profile email https://www.googleapis.com/auth/spreadsheets.readonly'
+        render={(renderProps) => (
+          // <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+          <Flex
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+            cursor='pointer'
+            alignItems='center'
+            justifyContent='center'
+            maxW='sm'
+            p='3'
+            borderWidth='2px'
+            rounded='md'
+            mt='4'
+          >
+            <Icon as={FcGoogle} mr={4} w={10} h={10} />
+            <Heading size='md'>Connect to Google</Heading>
+          </Flex>
+        )}
+      />
+      {error && <Text color='red.600'>{error}</Text>}
+    </>
   );
 };
 
